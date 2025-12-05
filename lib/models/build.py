0bb74350cc46fd31c26601558539990c6e01cb7a
Raghvendra Gupta <raghr14@gmail.com>
2025-12-05 22:36:06 +0530

    Import new CNN models for registration
    Added import statements for new CNN models.

--- lib/models/build.py
+++ lib/models/build.py
@@ -1,8 +1,13 @@
 from .registry import model_entrypoints
 from .registry import is_model
-# Import your new CNN model
-from .vanilla_cnn import VanillaCNN
+#CNN+ Import your new CNN model
 
+# Import all models here to register them with the MODEL_REGISTRY
+from .vanilla_cnn import VanillaCNN 
+from .cls_cvt import CvT
+# The build_model function is imported from the builder
+from .builder import build_model
+#CNN-
 def build_model(config, **kwargs):
     model_name = config.MODEL.NAME
     if not is_model(model_name):
