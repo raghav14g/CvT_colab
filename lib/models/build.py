841b821862da5f35b1b55792a666719fb03687df
Raghvendra Gupta <raghr14@gmail.com>
2025-12-08 00:07:01 +0530

    Refactor build.py by removing build_model function
    Removed the build_model function and updated import statements.

--- lib/models/build.py
+++ lib/models/build.py
@@ -1,16 +1,18 @@
 from .registry import model_entrypoints
 from .registry import is_model
-#CNN+ Import your new CNN model
+#CNN+Added Import your new CNN model
 
 # Import all models here to register them with the MODEL_REGISTRY
-from .vanilla_cnn import VanillaCNN 
-from .cls_cvt import CvT
+from .vanilla_cnn import vanilla_cnn 
+from .cls_cvt import get_cls_model
 # The build_model function is imported from the builder
 from .builder import build_model
-#CNN-
-def build_model(config, **kwargs):
-    model_name = config.MODEL.NAME
-    if not is_model(model_name):
-        raise ValueError(f'Unkown model: {model_name}')
-
-    return model_entrypoints(model_name)(config, **kwargs)
+#CNN-Added
+#CNN+Removed
+#def build_model(config, **kwargs):
+#    model_name = config.MODEL.NAME
+#    if not is_model(model_name):
+#        raise ValueError(f'Unkown model: {model_name}')
+#
+#    return model_entrypoints(model_name)(config, **kwargs)
+#CNN-Removed
